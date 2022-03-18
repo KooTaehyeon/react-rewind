43bd5794b80dc00666ce36933454e173111b9842
Kth <vcro12123@naver.com>
2022-03-18 16:43:30 +0900

    Context API 학습

--- src/App.js
+++ src/App.js
@@ -53,6 +53,8 @@ function reducer(state, action) {
       return state;
   }
 }
+// UserDispatch 라는 이름으로 내보내줍니다.
+export const UserDispatch = React.createContext(null);
 
 function App() {
   const [{ username, email }, onChange, reset] = useInputs({
@@ -77,32 +79,20 @@ function App() {
     nextId.current += 1;
   }, [username, email, reset]);
 
-  const onToggle = useCallback((id) => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id,
-    });
-  }, []);
-
-  const onRemove = useCallback((id) => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id,
-    });
-  }, []);
-
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
-      <div>활성사용자 수 : {count}</div>
+      <UserDispatch.Provider value={dispatch}>
+        <CreateUser
+          username={username}
+          email={email}
+          onChange={onChange}
+          onCreate={onCreate}
+        />
+        <UserList users={users} />
+        <div>활성사용자 수 : {count}</div>
+      </UserDispatch.Provider>
     </>
   );
 }
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { UserDispatch } from '../App';
+import useInputs from '../hooks/useInputs';
+const CreateUser = ({ users }) => {
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: '',
+    email: '',
+  });
+  const nextId = useRef(4);
+  const dispatch = useContext(UserDispatch);
 
-const CreateUser = ({ username, email, onChange, onCreate }) => {
+  const onCreate = () => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
+    reset();
+    nextId.current += 1;
+  };
+
   return (
     <div>
       <input
+        name='username'
         placeholder='계정명'
         onChange={onChange}
         value={username}
-        name={'username'}
       />
       <input
+        name='email'
         placeholder='이메일'
         onChange={onChange}
         value={email}
-        name='email'
       />
       <button onClick={onCreate}>버튼</button>
     </div>
